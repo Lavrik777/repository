ba13cfda8866cb63da2d12fb8e64e1e4298ca6ec
lavrik777 <lavrik77777@gmail.com>
2018-07-10 00:04:29 +0300

    Добавил привет

--- index.php
+++ index.php
@@ -1,4 +1,146 @@
 <?
+echo "<pre>";
 echo "Hello World";
-echo "Привет"
+
+$b = 1.1;
+$c = "Leonid";
+$d = [1, 2, 3, 'name' => "Leonid"];
+$e = true;
+
+
+
+
+
+echo '<pre>';
+var_dump($a, $b, $c, $d);
+echo $d[0] . '</br>';
+echo $d['name'] . '</br>';
+
+echo '<pre>';
+
+echo '<pre>';
+var_dump($a, $b, $c, $d, $e);
+echo $d[0] . '</br>';
+echo $d['name'] . '</br>';
+
+
+
+$name = "Leonid";
+
+
+echo "<p> Привет $name</p>";
+echo "<p> Привет . $name . '!'</p>";
+
+$str = 'This isn\'t string';
+echo $str;
+
+
+
+$a = 2; // целое число
+$aa = 2;
+echo $a + $aa . '</br>';
+var_dump($a/$aa);
+
+echo "Сложение" . ($a + $aa) . "</br>";
+
+
+
+const MY_NAME = "Leonid Petrovich";
+define("FOO", "что-тоэ");
+$a = 1; // Целое число
+$b = 1.1; // Плавающая точка
+$c = "Leonid"; // Строка
+$d = [1, 2, 3, "name" => "Vadim"]; // Массив
+$e = true;
+
+var_dump($a, $b, $c, $d, $e);
+echo $d [0] . "</br>";
+echo $d ["name"] . "</br>";
+echo "</pre>";
+//однострочный комментарий # это также однострочный комментарий
+/*это многострочный
+комментарий
+здесь не будут
+выполняться команды */
+
+
+echo "<p> Привет мир! </p>";
+$name = "Леонид";
+echo "<p> Привет $name! </p>";
+echo "<p> Привет {$name}s! </p>";
+echo ($a += $b) . "</br>"; // $a = $a +b
+echo MY_NAME;
+
+
+
+$day = 8;
+if (!($day % 2)) {
+    echo "Good";
+} else {
+    echo "Bad";
+}
+
+
+$a = 1;
+$b = 2;
+
+
+switch ($i){
+   case 0:
+        echo "i равно 0";
+        break;
+     case 1:
+        echo "i равно 1";
+        break;
+     case 2:
+        echo "i равно 2";
+        break;
+    default:
+        echo "i не равно 0, 1 или 2";
+}
+
+$day = "Понедельник";
+$date = 17;
+
+if ($day == "Понедельник" && $date == 16) {
+    echo "Будет англ.";
+} else {
+    echo " учим PHP";
+}
+echo '<pre>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
